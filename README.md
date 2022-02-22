0b272c8d38214f0cf596cf078e016b61bcf3eae1
Goutham S <gouthamshine@gmail.com>
2022-02-22 23:58:49 +0530

    update to use HTTPS in localhost

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # auth-server
+
 A NodeJS based Auth Server
 
+### Steps for run this app in 🔐`HTTPS` mode
+
+This is now required as part of this commit because I've added `helmet` 🪖 <br> A dependency which enforces additional security
+
+- > Create a directory named `cert` under `src` <br> navigate to this `cert` directory
+
+- > Create `key.pem` file <br> `openssl genrsa -out key.pem`
+
+- > Create `csr.pem` file <br> `openssl req -new -key key.pem -out csr.pem`
+
+- > Create `cert.pem` file <small>actual certificate file</small> <br> `openssl x509 -req -days 365 -in csr.pem -signkey key.pem -out cert.pem`
+
+**NOTE:** openssl comes pre-installed on MAC and easy to get it on any linux distros.
+
+So, for windows buddies hit [this link](https://stackoverflow.com/questions/50625283/how-to-install-openssl-in-windows-10), which takes you to a stack-overflow answer, which might help setting up openssl and probably get your certificates generated with the same commands as above _<small>(don't forget to create and navigate to the `cert` directory within `src`)<small>_
+
 ### License
+
 [MIT](https://gouthamshiv.github.io/mit-license)
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import https from 'https';
 import express from 'express';
 import config from 'config';
-// import helmet from 'helmet';
+import helmet from 'helmet';
 import log from './utils/logger';
 import router from './routes/index';
 import connectToDB from './utils/db.connect';
 
 const server = express().disable('x-powered-by');
-// server.use(helmet());
+server.use(helmet());
 server.use(express.json());
 server.use(router);
 
-const PORT = config.get('port');
+// const PORT = config.get('port');
 
-server.listen(PORT, () => {
-  log.info(`Server started at http://localhost:${PORT}`);
+// server.listen(PORT, () => {
+//   log.info(`Server started at http://localhost:${PORT}`);
+//   connectToDB();
+// });
+
+if (!process.env.PORT) {
+  const sslServer = https.createServer(
+    {
+      key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+      cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+    },
+    server,
+  );
+  sslServer.listen(8082, () => {
+    log.info('🔐 Server successfully started https://localhost:8082');
+  });
   connectToDB();
-});
+} else {
+  const { PORT } = process.env;
+  server.listen(PORT, () => {
+    log.info(`🔐 Server successfully started on port ${PORT}`);
+  });
+  connectToDB();
+}
